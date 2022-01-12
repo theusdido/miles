df24d5f8998a1c28a063bee0a6a2c63dd0177055
Edilson Bitencourt <edilsonbitencourt@hotmail.com>
2022-01-12 11:08:57 -0300

    extrato locativa

--- core/nodejs/index.js
+++ core/nodejs/index.js
@@ -80,7 +80,7 @@ app.get('/pesquisarextrato', cors(), function(req,res){
         }
 
         if (!is_empty(params.dataliberacao)){
-            filtros['titulo.datageracao'] = params.dataliberacao;
+            filtros['titulo.data_geracao'] = params.dataliberacao;
         }
 
         if (!is_empty(params.titulo)){
@@ -98,16 +98,20 @@ app.get('/pesquisarextrato', cors(), function(req,res){
             filtros['cabecalho.gerador'] = { $in: geracao};
         }
 
-        let p_formapagamento = params['formapagamento[]'];
+        let p_formapagamento = params['forma_pagamento[]'];
         if (!is_empty(p_formapagamento)){
             let formapagamento  = typeof p_formapagamento === 'string' ? [p_formapagamento] : p_formapagamento;
-            filtros['formapagamento.codigo'] = { $in: formapagamento};
+            filtros['forma_pagamento.codigo'] = { $in: formapagamento};
         }
 
         let p_negativo = params['negativo'];
         if (!is_empty(p_negativo) && p_negativo != '-1'){
-            filtros['cabecalho.isnegativo'] = p_negativo;
+            filtros['cabecalho.is_negativo'] = p_negativo;
         }
+
+        // Filtro inativos
+        filtros['cabecalho.inativo'] = false;
+
         dbo.collection('extrato').find(filtros).toArray(function(err,result){
             retorno = result;
             resposta.json(retorno);
