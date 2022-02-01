e7fdccae001ea1b2b21c96d609513e3f965c4528
theusdido <edilsonbitencourt@hotmail.com>
2022-02-01 18:35:41 -0300

    transportadora ecommerce

--- core/classes/ecommerce/transportadora.class.php
+++ core/classes/ecommerce/transportadora.class.php
@@ -4,11 +4,11 @@ class Transportadora {
     // Retorna lista de Transportadoras
     public static function getTransportadoras(){
         $sql = "SELECT * FROM td_ecommerce_transportadora WHERE inativo <> 1;";
-        $query = Conexao::get()->query($sql);
+        $query = Transacao::get()->query($sql);
         return $query->fetchAll();
     }
+
     public static function getLogo($transportora){
-        #return $_SESSION["PATH_TDECOMMERCE"] . "/img/icon-entrega-motoboy.png";
         return $_SESSION["PATH_TDECOMMERCE"] . "/img/beedelivery/logo.png";
     }
 }
