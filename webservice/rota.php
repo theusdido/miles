1089fc933bddd8ad80e5b208305d3f93d9139ac0
theusdido <theusdido@hotmail.com>
2022-09-03 16:50:53 -0300

    atualizando webservice para a sidercomp

--- webservice/rota.php
+++ webservice/rota.php
@@ -1,4 +1,9 @@
-<?php	
+<?php
+	$_service = tdClass::Read("service");
+	if ($_service == ''){
+		echo 'Serviço não especificado';
+		exit;
+	}
 	try {
 		$servico = str_replace(".","/",tdClass::Read("service"));
 		require_once "servicos/{$servico}.php";
