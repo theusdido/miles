04ec8d74dc053e1edaf0c0163a98c45361daefb3
theusdido <theusdido@hotmail.com>
2022-11-07 04:33:05 -0300

    Corrigindo Path de instalação

--- index.php
+++ index.php
@@ -5,9 +5,5 @@
 	// Define o ambiente como sistema
 	define('AMBIENTE','SISTEMA');
 
-	ini_set('display_errors',1);
-	ini_set('display_startup_erros',1);
-	error_reporting(E_ALL);
-
 	// Carrega a biblioteca MILES FRAMEWORK PHP
-	require 'vendor/theusdido/miles-library/autoload.php';
+	require __DIR__ . '/vendor/theusdido/miles-library/autoload.php';
