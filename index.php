1bd7b0ec0bbde90df718fefb12e220f725a864da
theusdido <theusdido@hotmail.com>
2022-11-07 03:10:38 -0300

    Processo de instalação funcionando

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
 	// Define o ambiente como sistema
 	define('AMBIENTE','SISTEMA');
 
+	ini_set('display_errors',1);
+	ini_set('display_startup_erros',1);
+	error_reporting(E_ALL);
+
 	// Carrega a biblioteca MILES FRAMEWORK PHP
-	require 'core/autoload.php';
+	require 'vendor/theusdido/miles-library/autoload.php';
